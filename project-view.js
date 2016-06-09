345390ea953f1d62288e4ea6db966d07eaaf9c37
dylanjsa90 <dylanjsanders1@gmail.com>
2016-06-08 23:28:40 -0700

    added about me section, project-view.js for events and navigation handling added blank filters select menus and started the code for populating those filters

---
+++ project-view.js
@@ -0,0 +1,44 @@
+var projectView = {};
+
+projectView.addFilters = function() {
+  $('article').not('.template').each(function() {
+    console.log($(this));
+    var filter = $('"' + $(this) + '":first-child').text();
+    console.log(filter);
+    var optionTag = '<option value"' + filter + '">' + filter + '</option>';
+    $('#a-filter').append(optionTag);
+
+
+  });
+};
+
+projectView.handleNav = function() {
+  $('.main-nav').on('click', '.tab', function() {
+    $('.tab-content').hide();
+    var content = $(this).attr('data-content');
+    $('#' + content).fadeIn();
+  });
+  $('.main-nav .tab:first').click();
+};
+
+projectView.showMore = function() {
+  $('.project-body *:nth-of-type(n+2)').hide();
+  $('#projects').on('click', 'a.show-more', function() {
+    event.preventDefault();
+    $(this).parent().find('*').fadeIn('fast');
+    $(this).text('Show less').attr('class', 'show-less').removeClass('show-more');
+  });
+
+  $('#projects').on('click', 'a.show-less', function() {
+    event.preventDefault();
+    $('.project-body *:nth-of-type(n+2)').hide();
+    $(this).text('Show more').attr('class', 'show-more').removeClass('show-less');
+
+  });
+};
+
+$(document).ready(function() {
+  projectView.addFilters();
+  projectView.handleNav();
+  projectView.showMore();
+});
