2c22fe99cffa29d8ee02ad9a130027c72b01c724
dylanjsa90 <dylanjsanders1@gmail.com>
2016-06-14 15:24:37 -0700

    added stats to bottom of index page, enclosed .js files with IIFE, refactored some data retrieval, calculated some project stats and hid filters until I decide how to filter

--- scripts/app.js
+++ scripts/app.js
@@ -1,35 +1,54 @@
-function Project(opts) {
-  for (keys in opts) {
-    this[keys] = opts[keys];
+(function(module) {
+
+  function Project(opts) {
+    for (keys in opts) {
+      this[keys] = opts[keys];
+    }
   }
-}
 
-Project.all = [];
+  Project.allProjects = [];
 
-Project.prototype.toHtml = function (scriptTemplateId) {
-  this.publishedTime = parseInt((new Date() - new Date(this.publishedOn))/ 60 / 60 / 24 / 1000) + ' days ago.';
-  var template = Handlebars.compile($(scriptTemplateId).text());
-  return template(this);
-};
+  Project.prototype.toHtml = function (scriptTemplateId) {
+    this.publishedTime = parseInt((new Date() - new Date(this.publishedOn))/ 60 / 60 / 24 / 1000) + ' days ago.';
+    var template = Handlebars.compile($(scriptTemplateId).text());
+    return template(this);
+  };
 
-Project.loadAll = function(dataWePassIn) {
-  dataWePassIn.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  }).forEach(function(ele) {
-    Project.all.push(new Project(ele));
-  });
-};
+  Project.loadAll = function(dataWePassIn) {
+    Project.allProjects = dataWePassIn.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    })
+    .map(function(ele) {
+      return new Project(ele);
+    });
+  };
 
-Project.fetchAll = function() {
-  if (localStorage.fakeProjectData) {
-    var data = JSON.parse(localStorage.getItem('fakeProjectData'));
-    Project.loadAll(data);
-    projectView.renderIndexPage();
-  } else {
+  Project.fetchAll = function(nextFunction) {
+    if (localStorage.fakeProjectData) {
+      var data = JSON.parse(localStorage.getItem('fakeProjectData'));
+      Project.loadAll(data);
+      nextFunction();
+    } else {
+      Project.getAll(nextFunction);
+    };
+  };
+
+  Project.getAll = function(nextFunction) {
     $.getJSON('../data/fakeprojectdata.json', function(data) {
       localStorage.setItem('fakeProjectData', JSON.stringify(data));
       Project.loadAll(data);
-      projectView.renderIndexPage();
+      nextFunction();
     });
-  }
-};
+  };
+
+  Project.numWordsAll = function() {
+    return Project.allProjects.map(function(project) {
+      return project.description.match(/\w+/g).length;
+    })
+    .reduce(function(a, b) {
+      return a + b;
+    });
+  };
+
+  module.Project = Project;
+})(window);
--- scripts/project-view.js
+++ scripts/project-view.js
@@ -1,47 +1,55 @@
-var projectView = {};
+(function(module) {
+  var projectView = {};
 
-projectView.addFilters = function() {
-  $('article').not('.template').each(function() {
-    console.log($(this));
-    // var filter = $('"' + $(this) + '":first-child').text();
-    // console.log(filter);
-    // var optionTag = '<option value"' + filter + '">' + filter + '</option>';
-    // $('#a-filter').append(optionTag);
-  });
-};
+  projectView.addFilters = function() {
+    $('article').not('.template').each(function() {
+      console.log($(this));
+      // var filter = $('"' + $(this) + '":first-child').text();
+      // console.log(filter);
+      // var optionTag = '<option value"' + filter + '">' + filter + '</option>';
+      // $('#a-filter').append(optionTag);
+    });
+  };
 
-projectView.handleNav = function() {
-  $('.main-nav').on('click', '.tab', function() {
-    $('.tab-content').hide();
-    var content = $(this).attr('data-content');
-    $('#' + content).fadeIn();
-  });
-  $('.main-nav .tab:first').click();
-};
+  projectView.handleNav = function() {
+    $('.main-nav').on('click', '.tab', function() {
+      $('.tab-content').hide();
+      var content = $(this).attr('data-content');
+      $('#' + content).fadeIn();
+    });
+    $('.main-nav .tab:first').click();
+  };
 
-projectView.showMore = function() {
-  $('.project-body *:nth-of-type(n+2)').hide();
-  $('#projects').on('click', 'a.show-more', function() {
-    event.preventDefault();
-    $(this).parent().find('*').fadeIn('fast');
-    $(this).text('Show less').attr('class', 'show-less').removeClass('show-more');
-  });
-
-  $('#projects').on('click', 'a.show-less', function() {
-    event.preventDefault();
+  projectView.showMore = function() {
     $('.project-body *:nth-of-type(n+2)').hide();
-    $(this).text('Show more').attr('class', 'show-more').removeClass('show-less');
+    $('#projects').on('click', 'a.show-more', function() {
+      event.preventDefault();
+      $(this).parent().find('*').fadeIn('fast');
+      $(this).text('Show less').attr('class', 'show-less').removeClass('show-more');
+    });
 
-  });
-};
+    $('#projects').on('click', 'a.show-less', function() {
+      event.preventDefault();
+      $('.project-body *:nth-of-type(n+2)').hide();
+      $(this).text('Show more').attr('class', 'show-more').removeClass('show-less');
 
-projectView.renderIndexPage = function() {
-  Project.all.forEach(function(a) {
-    $('#projects').append(a.toHtml('#article-template'));
-  });
-  projectView.addFilters();
-  projectView.handleNav();
-  projectView.showMore();
-};
+    });
+  };
 
-Project.fetchAll();
+  projectView.renderIndexPage = function() {
+    Project.allProjects.forEach(function(a) {
+      $('#projects').append(a.toHtml('#article-template'));
+    });
+    projectView.addFilters();
+    projectView.handleNav();
+    projectView.showMore();
+    $('.total-projects').text(Project.allProjects.length);
+    $('.total-words').text(Project.numWordsAll());
+    $('.tapp').text(Project.numWordsAll() / Project.allProjects.length);
+  };
+
+
+  Project.fetchAll(projectView.renderIndexPage);
+
+  module.projectView = projectView;
+})(window);
